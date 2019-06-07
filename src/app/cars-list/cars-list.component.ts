78e98c7d628dd3c1decd6a3357b68db064b1cc22
Karim KOLLI <karim.kolli@gmail.com>
2019-06-07 09:57:23 +0200

    upgrade angular material 8 and flex-layout

--- src/app/cars-list/cars-list.component.ts
+++ src/app/cars-list/cars-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router }  from '@angular/router';
-import { MatSort, MatTableDataSource } from '@angular/material';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 import { CarsService } from './../cars.service';
 import { ICar } from './../car';
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
-import {
-  MatButtonModule,
-  MatTableModule,
-  MatChipsModule,
-  MatProgressSpinnerModule,
-  MatCardModule,
-  MatSortModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
 
 const Material = [
   MatButtonModule,
