c8ebb1a6eb05db419d94f3b12cf0c8496e8c4aeb
Karim KOLLI <karim.kolli@gmail.com>
2019-08-15 19:41:22 +0200

    carservice with json-server

--- src/app/cars-list/cars-list.component.ts
+++ src/app/cars-list/cars-list.component.ts
@@ -3,6 +3,7 @@ import { Router }  from '@angular/router';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 
+
 import { CarsService } from './../cars.service';
 import { ICar } from './../car';
 
@@ -44,6 +45,8 @@ export class CarsListComponent implements OnInit  {
 
 
   carsLoading = false;
+
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(private router: Router, private _carService: CarsService) { }
 
